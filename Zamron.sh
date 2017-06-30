678f506fbbb8922a9318bbdd243fd64179887881
Bhachchaj <bhachchaj@users.noreply.github.com>
2017-06-30 02:29:09 -0700

    Update Zamron.sh

--- Zamron.sh
+++ Zamron.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Welcome to Zamron PDF Generator. Just lead Zamron to the folder which contains the Archives, and let Zamron do the rest."
-echo "Just to be safe, create a new folder with the archived files. Warning! The archives will be deleted after the generation of the pdf's." 
+echo "Welcome to Zamron PDF Generator. Just lead Zamron to the folder which contains the Archives, and let Zamron do the rest. Copyright 2017 Bhachchaj."
+echo "Warning! The archives will be deleted after the generation of the pdf's." 
 echo -n "Enter the Path to the Folder: "
 read text
 cd $text
